3a8fa0abfe88f326b81d30187d11dc7484a459df
zemaf <enaccf31400@gmail.com>
2022-02-12 14:07:05 +0100

    Version 1.1 : import liste artisans du tableau excel liste_Pros_Occ

--- src/main/python/package/api/constants.py
+++ src/main/python/package/api/constants.py
@@ -1,20 +1,19 @@
 import os
 from pathlib import Path
+import pandas as pd
+from pprint import PrettyPrinter
 
 # pour éviter les pbs de permissions on va stocker les notes dans le dossier utilisateur
 # os.path.join(chemin, str) concatène 'chemin' et 'str' pour fabriquer un chemin intelligible sur mac, windows ou linux
 MES_PROSPECTS = os.path.join(Path.home(), "MES_PROSPECTS")  # Path.home() = dossier de l'utilisateur courant
 
-LISTE_ARTISANS = {'Th.Bégué': {'email': 'th.begue@free.fr',
-                               'tel': '33603528332'
-                               },
-                  'Kiisol': {'email': 'contact@kiisol.fr',
-                            'tel': '33610937009'
-                             },
-                  'Poweet': {'email': 'tinoveler@gmail.com',
-                             'tel': '33650591900'
-                             }
-                  }
+liste_artisans = pd.read_excel('liste_Pros_occ.xlsx', 'listing', dtype=object)
+liste_artisans = liste_artisans.set_index('NOM')
+
+LISTE_ARTISANS = liste_artisans.to_dict('index')
+
+pp = PrettyPrinter(sort_dicts=True)
+pp.pprint(LISTE_ARTISANS)
 # liste = {'nom': 'Poweet', 'prenom': 'Kiisol'}
 # for k in liste.values():
 #     print(LISTE_ARTISANS[k]['tel'])
