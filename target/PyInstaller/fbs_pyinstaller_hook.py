70dee0e8cf437cfbc7a5d6f7619e8bdebd589c7a
zemaf <enaccf31400@gmail.com>
2024-12-25 13:31:46 +0100

    remove target/ from git tracking

--- target/PyInstaller/fbs_pyinstaller_hook.py
+++
@@ -1,3 +0,0 @@
-import importlib
-module = importlib.import_module('fbs_runtime._frozen')
-module.PUBLIC_SETTINGS = {'app_name': 'Les_Pros_Occitans', 'author': 'Imaf', 'version': '0.0.0', 'environment': 'local'}
